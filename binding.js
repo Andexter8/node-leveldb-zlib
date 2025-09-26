f4125dce08869608377181e25537f1efd61c8c06
8Crafter (PC) <8crafteryt@gmail.com>
2025-09-26 10:03:35 -0700

    Added the ability to read node-leveldb binaries when the version portion of the path does not exactly match the version of the OS. - Added the ability to read node-leveldb binaries when the version portion of the path does not exactly match the version of the OS, it will try to find an exact match, and if it can't it will try to find the closest match to the version, that does exactly match the OS type and architecture. - Added the ability to override the architecture name used in the path with the LEVELDB_ZLIB_ARCH_OVERRIDE environment variable.

--- binding.js
+++ binding.js
@@ -1,31 +1,55 @@
-const helper = require('./helpers/buildPath.js')
-const path = require('path')
-const debug = require('debug')('leveldb')
+const helper = require("./helpers/buildPath.js");
+const path = require("path");
+const debug = require("debug")("leveldb");
 
 if (!process.versions.electron) {
-  // Electron has its own crash handler, and segfault-handler
-  // uses NAN which is a hassle, so only load outside electron
-  try {
-    const SegfaultHandler = require('segfault-handler')
-    SegfaultHandler.registerHandler('crash.log')
-  } catch (e) {
-    debug('[leveldb] segfault handler is not installed. If you run into crashing issues, install it with `npm i -D segfault-handler` to get debug info on native crashes')
-  }
+    // Electron has its own crash handler, and segfault-handler
+    // uses NAN which is a hassle, so only load outside electron
+    try {
+        const SegfaultHandler = require("segfault-handler");
+        SegfaultHandler.registerHandler("crash.log");
+    } catch (e) {
+        debug(
+            "[leveldb] segfault handler is not installed. If you run into crashing issues, install it with `npm i -D segfault-handler` to get debug info on native crashes"
+        );
+    }
 }
 
-let bindings
-const pathToSearch = helper.getPath()
+let bindings;
+const pathToSearch = helper.getPath(!process.env.VERSIONLESS_LEVELDB_ZLIB);
 if (pathToSearch) {
-  const rpath = path.join(__dirname, pathToSearch, '/node-leveldb.node')
-  try {
-    bindings = require(rpath)
-  } catch (e) {
-    debug(e)
-    debug('[leveldb] did not find lib in ', rpath)
-  }
+    const rpath = path.join(__dirname, pathToSearch, "node-leveldb.node");
+    try {
+        bindings = require(rpath);
+    } catch (e) {
+        debug(e);
+        debug("[leveldb] did not find lib in ", rpath);
+        const rpathContainer = path.join(__dirname, pathToSearch, "../");
+        try {
+            const fs = require("fs");
+            const files = fs.readdirSync(rpathContainer).filter((f) => fs.statSync(path.join(rpathContainer, f)).isDirectory());
+            const mostSimilar = helper.findMostSimilarPath(files);
+            if (mostSimilar) {
+                const rpathMostSimilar = path.join(rpathContainer, mostSimilar, "node-leveldb.node");
+                try {
+                    bindings = require(rpathMostSimilar);
+                    debug("[leveldb] loaded lib from most similar folder ", rpathMostSimilar);
+                } catch (e2) {
+                    debug(e2);
+                    debug("[leveldb] did not find lib in ", rpathMostSimilar);
+                }
+            } else {
+                debug("[leveldb] did not find any similar folder in ", rpathContainer);
+            }
+        } catch (e2) {
+            debug(e2);
+            debug("[leveldb] did not find matching subfolder with lib in ", rpathContainer);
+        }
+    }
 }
 if (!bindings) {
-  bindings = require('bindings')('node-leveldb.node')
+    bindings = require("bindings")("node-leveldb.node");
 }
 
-module.exports = bindings
+module.exports = bindings;
+
--- helpers/buildPath.js
+++ helpers/buildPath.js
@@ -1,23 +1,69 @@
-const os = require('os')
+const os = require("os");
+const path = require("node:path");
 
 module.exports = {
-  getPath () {
-    const _osVersion = os.release()
+    getPath(includeVersion = true) {
+        const _osVersion = os.release();
 
-    const plat = process.platform
-    const arch = process.arch
-    const ver = _osVersion.split('.', 1)
+        const plat = process.platform;
+        const arch = process.env.LEVELDB_ZLIB_ARCH_OVERRIDE || process.arch;
+        const ver = _osVersion.split(".", 1);
 
-    const bpath = `./prebuilds/${plat}-${ver}-${arch}/`
-    return bpath
-  },
+        const bpath = `./prebuilds/${plat}${includeVersion ? `-${ver}` : ""}-${arch}/`;
+        return bpath;
+    },
 
-  getPlatformString () {
-    const _osVersion = os.release()
+    /**
+     *
+     * @param {string[]} paths
+     * @return {string | null}
+     */
+    findMostSimilarPath(paths) {
+        const _osVersion = os.release();
 
-    const plat = process.platform
-    const arch = process.arch
-    const ver = _osVersion.split('.', 1)
-    return `${plat}-${ver}-${arch}`
-  }
-}
+        const plat = process.platform;
+        const arch = process.env.LEVELDB_ZLIB_ARCH_OVERRIDE || process.arch;
+        const ver = _osVersion.split(".", 1)[0];
+
+        const pathEnds = paths.map((p) => path.basename(p));
+
+        return pathEnds.reduce(
+            /**
+             *
+             * @param {string | null} previousPath
+             * @param {string} currentPath
+             * @returns {string | null}
+             */
+            (previousPath, currentPath) => {
+                const parts = currentPath.split("-");
+                const prevParts = previousPath?.split("-") ?? null;
+                let [platPart, verPart, archPart] = parts.length === 2 ? [parts[0], null, parts[1]] : parts;
+                let [prevPlatPart, prevVerPart, prevArchPart] =
+                    prevParts === null ? [] : prevParts.length === 2 ? [prevParts[0], null, prevParts[1]] : prevParts;
+                if (platPart !== plat) return previousPath;
+                if (archPart !== arch) return previousPath;
+                if (previousPath !== null && verPart !== null && verPart !== ver && (prevVerPart === null || prevVerPart === ver)) return previousPath;
+                if (verPart === null && prevVerPart === ver) return previousPath;
+                const verPartNum = Number(verPart);
+                const prevVerPartNum = Number(prevVerPart);
+                if (verPart === null && (previousPath === null || (prevVerPart !== null && prevVerPart !== ver))) return currentPath;
+                if (previousPath !== null && isNaN(verPartNum) && isNaN(prevVerPartNum)) return previousPath;
+                if (previousPath !== null && isNaN(verPartNum) && !isNaN(prevVerPartNum)) return previousPath;
+                if (previousPath !== null && !isNaN(verPartNum) && isNaN(prevVerPartNum)) return currentPath;
+                if (previousPath !== null && Math.abs(verPartNum - Number(ver)) < Math.abs(prevVerPartNum - Number(ver))) return currentPath;
+                return previousPath ?? currentPath;
+            },
+            null
+        ) ?? null;
+    },
+
+    getPlatformString(includeVersion = true) {
+        const _osVersion = os.release();
+
+        const plat = process.platform;
+        const arch = process.env.LEVELDB_ZLIB_ARCH_OVERRIDE || process.arch;
+        const ver = _osVersion.split(".", 1);
+        return `${plat}${includeVersion ? `-${ver}` : ""}-${arch}`;
+    },
+};
+
